2e1c860e513765ac5282170a50c89e57e50d707b
Leon Anavi <leon@anavi.org>
2015-03-02 04:40:11 +0200

    Send JSON with temperature over MQTT

--- esp8266-mqtt/user/user_main.c
+++ esp8266-mqtt/user/user_main.c
@@ -37,8 +37,24 @@
 #include "gpio.h"
 #include "user_interface.h"
 #include "mem.h"
+#include "os_type.h"
+
+#define user_procTaskPrio        0
+#define user_procTaskQueueLen    1
 
 MQTT_Client mqttClient;
+
+int g_temperature = 0;
+int g_thresholdTemperature = 30;
+
+os_event_t    user_procTaskQueue[user_procTaskQueueLen];
+static void user_procTask(os_event_t *events);
+
+static volatile os_timer_t some_timer;
+
+static int gpioPin;
+
+char* itoa(int value, char* result, int base);
 
 void wifiConnectCb(uint8_t status)
 {
@@ -53,15 +69,6 @@ void mqttConnectedCb(uint32_t *args)
 {
 	MQTT_Client* client = (MQTT_Client*)args;
 	INFO("MQTT: Connected\r\n");
-	MQTT_Subscribe(client, "/mqtt/topic/0", 0);
-	MQTT_Subscribe(client, "/mqtt/topic/1", 1);
-	MQTT_Subscribe(client, "/mqtt/topic/2", 2);
-
-	MQTT_Publish(client, "hello/world", "hello0", 6, 0, 0);
-	INFO("MQTT: hello/world published...\r\n");
-	MQTT_Publish(client, "/mqtt/topic/1", "hello1", 6, 1, 0);
-	MQTT_Publish(client, "/mqtt/topic/2", "hello2", 6, 2, 0);
-
 }
 
 void mqttDisconnectedCb(uint32_t *args)
@@ -94,6 +101,226 @@ void mqttDataCb(uint32_t *args, const char* topic, uint32_t topic_len, const cha
 	os_free(dataBuf);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+void ICACHE_FLASH_ATTR ds_init( int gpio )
+{
+	//set gpio2 as gpio pin
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+	//disable pulldown
+	PIN_PULLDWN_DIS(PERIPHS_IO_MUX_GPIO2_U);
+	//enable pull up R
+	PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO2_U);
+	// Configure the GPIO with internal pull-up
+	// PIN_PULLUP_EN( gpio );
+	GPIO_DIS_OUTPUT( gpio );
+	gpioPin = gpio;
+}
+
+// Perform the onewire reset function.  We will wait up to 250uS for
+// the bus to come high, if it doesn’t then it is broken or shorted
+// and we return;
+
+void ICACHE_FLASH_ATTR ds_reset(void)
+{
+	//    IO_REG_TYPE mask = bitmask;
+	//    volatile IO_REG_TYPE *reg IO_REG_ASM = baseReg;
+	uint8_t retries = 125;
+	// noInterrupts();
+	// DIRECT_MODE_INPUT(reg, mask);
+	GPIO_DIS_OUTPUT( gpioPin );
+	// interrupts();
+	// wait until the wire is high… just in case
+	do {
+		//if (–retries == 0) return;
+		os_delay_us(2);
+	} while ( !GPIO_INPUT_GET( gpioPin ));
+	// noInterrupts();
+	GPIO_OUTPUT_SET( gpioPin, 0 );
+	// DIRECT_WRITE_LOW(reg, mask);
+	// DIRECT_MODE_OUTPUT(reg, mask);    // drive output low
+	// interrupts();
+	os_delay_us(480);
+	// noInterrupts();
+	GPIO_DIS_OUTPUT( gpioPin );
+	// DIRECT_MODE_INPUT(reg, mask);    // allow it to float
+	os_delay_us(70);
+	// r = !DIRECT_READ(reg, mask);
+	//r = !GPIO_INPUT_GET( gpioPin );
+	// interrupts();
+	os_delay_us(410);
+}
+
+//
+// Write a bit. Port and bit is used to cut lookup time and provide
+// more certain timing.
+//
+static inline void write_bit( int v )
+{
+	// IO_REG_TYPE mask=bitmask;
+	//    volatile IO_REG_TYPE *reg IO_REG_ASM = baseReg;
+	GPIO_OUTPUT_SET( gpioPin, 0 );
+	if( v ) {
+		// noInterrupts();
+		//    DIRECT_WRITE_LOW(reg, mask);
+		//    DIRECT_MODE_OUTPUT(reg, mask);    // drive output low
+		os_delay_us(10);
+		GPIO_OUTPUT_SET( gpioPin, 1 );
+		// DIRECT_WRITE_HIGH(reg, mask);    // drive output high
+		// interrupts();
+		os_delay_us(55);
+	} else {
+		// noInterrupts();
+		//    DIRECT_WRITE_LOW(reg, mask);
+		//    DIRECT_MODE_OUTPUT(reg, mask);    // drive output low
+		os_delay_us(65);
+		GPIO_OUTPUT_SET( gpioPin, 1 );
+		//    DIRECT_WRITE_HIGH(reg, mask);    // drive output high
+		//        interrupts();
+		os_delay_us(5);
+	}
+}
+
+//
+// Read a bit. Port and bit is used to cut lookup time and provide
+// more certain timing.
+//
+static inline int read_bit(void)
+{
+	//IO_REG_TYPE mask=bitmask;
+	//volatile IO_REG_TYPE *reg IO_REG_ASM = baseReg;
+	int r;
+	// noInterrupts();
+	GPIO_OUTPUT_SET( gpioPin, 0 );
+	// DIRECT_MODE_OUTPUT(reg, mask);
+	// DIRECT_WRITE_LOW(reg, mask);
+	os_delay_us(3);
+	GPIO_DIS_OUTPUT( gpioPin );
+	// DIRECT_MODE_INPUT(reg, mask);    // let pin float, pull up will raise
+	os_delay_us(10);
+	// r = DIRECT_READ(reg, mask);
+	r = GPIO_INPUT_GET( gpioPin );
+	// interrupts();
+	os_delay_us(53);
+	return r;
+}
+
+//
+// Write a byte. The writing code uses the active drivers to raise the
+// pin high, if you need power after the write (e.g. DS18S20 in
+// parasite power mode) then set ‘power’ to 1, otherwise the pin will
+// go tri-state at the end of the write to avoid heating in a short or
+// other mishap.
+//
+void ICACHE_FLASH_ATTR  ds_write( uint8_t v, int power ) {
+	uint8_t bitMask;
+	for (bitMask = 0x01; bitMask; bitMask <<= 1) {
+		write_bit( (bitMask & v)?1:0);
+	}
+	if ( !power) {
+		// noInterrupts();
+		GPIO_DIS_OUTPUT( gpioPin );
+		GPIO_OUTPUT_SET( gpioPin, 0 );
+		// DIRECT_MODE_INPUT(baseReg, bitmask);
+		// DIRECT_WRITE_LOW(baseReg, bitmask);
+		// interrupts();
+	}
+}
+
+//
+// Read a byte
+//
+uint8_t ICACHE_FLASH_ATTR ds_read() {
+	uint8_t bitMask;
+	uint8_t r = 0;
+	for (bitMask = 0x01; bitMask; bitMask <<= 1) {
+		if ( read_bit()) r |= bitMask;
+	}
+	return r;
+}
+
+int get_temp()
+{
+	ds_reset();
+	ds_write(0xcc,1);
+	ds_write(0xbe,1);
+
+	int temperature=(int)ds_read();
+	temperature=temperature+(int)ds_read()*256;
+	temperature/=16;
+	if (temperature>100) {
+		temperature-=4096;
+	}
+
+	ds_reset();
+	ds_write(0xcc,1);
+	ds_write(0x44,1);
+	return temperature;
+}
+////////////////////////////////////////////////////////////////////////////////
+
+void some_timerfunc(void *arg)
+{
+	int currentTemperature = get_temp();
+	os_printf("temperature: %d \r\n", currentTemperature);
+
+	if (g_temperature == currentTemperature) {
+		//no need to change anything
+		return;
+	}
+
+	g_temperature = currentTemperature;
+
+	char *tempStr = "000.0";
+	itoa(g_temperature, tempStr, 10);
+
+	char str[255];
+	strcpy (str,"{ \"temperature\": ");
+	strcat (str,tempStr);
+	strcat (str,"} ");
+
+	MQTT_Client* client = (MQTT_Client*)arg;
+	MQTT_Publish(client, "/home/bee1/temperature", str, strlen(str), 0, 1);
+
+	if (g_temperature >= g_thresholdTemperature) {
+		// Set GPIO4 as high-level output,GPIO5 as low-level output
+		gpio_output_set(BIT4, BIT5, BIT4|BIT5, 0);
+	}
+	else {
+		// Set GPIO4 as high-level output,GPIO5 as low-level output
+		gpio_output_set(BIT5, BIT4, BIT5|BIT4, 0);
+	}
+}
+
+//Do nothing function
+static void ICACHE_FLASH_ATTR
+user_procTask(os_event_t *events)
+{
+	//os_delay_us(10);
+}
+
+char* itoa(int value, char* result, int base) {
+	// check that the base if valid
+	if (base < 2 || base > 36) { *result = (char) 0; return result; }
+
+	char* ptr = result, *ptr1 = result, tmp_char;
+	int tmp_value;
+
+	do {
+		tmp_value = value;
+		value /= base;
+		*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+	} while ( value );
+
+	// Apply negative sign
+	if (tmp_value < 0) *ptr++ = '-';
+	*ptr-- = (char) 0;
+	while(ptr1 < ptr) {
+		tmp_char = *ptr;
+		*ptr--= *ptr1;
+		*ptr1++ = tmp_char;
+	}
+	return result;
+}
 
 void user_init(void)
 {
@@ -120,8 +347,25 @@ void user_init(void)
 	MQTT_OnData(&mqttClient, mqttDataCb);
 
 	//WIFI_Connect(sysCfg.sta_ssid, sysCfg.sta_pwd, wifiConnectCb);
-	INFO("\r\nLet's connect\r\n");
 	WIFI_Connect("", "", wifiConnectCb);
 
-	INFO("\r\nSystem started ...\r\n");
+	// Initialize the GPIO subsystem.
+  gpio_init();
+
+  ds_init(2);
+  get_temp();
+
+	//Disarm timer
+	os_timer_disarm(&some_timer);
+
+	//Set GPIO5 to output mode
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
+
+	//Set GPIO4 to output mode
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4);
+
+	//// os_timer_setfn(ETSTimer *ptimer, ETSTimerFunc *pfunction, void *parg)
+	os_timer_setfn(&some_timer, (os_timer_func_t *)some_timerfunc, &mqttClient);
+	//// void os_timer_arm(ETSTimer *ptimer,uint32_t milliseconds, bool repeat_flag)
+	os_timer_arm(&some_timer, 1000, 1);
 }
