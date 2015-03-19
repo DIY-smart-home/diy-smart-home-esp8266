302d5e25e5671ed823332f3141a23476ed8d0b2a
Leon Anavi <leon@anavi.org>
2015-03-19 02:39:22 +0200

    Send ping request and read sensors data on startup

--- mobile/www/js/index.js
+++ mobile/www/js/index.js
@@ -18,7 +18,44 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+
+      $.ajax({ url: "http://iot.anavi.org/ping",
+        //Success callback
+        success: app.handlePingResponse,
+        error: app.requestError
+      });
+
+      app.receivedEvent('deviceready');
+    },
+
+    handlePingResponse : function(data) {
+        var result = JSON.parse(data);
+        if (result.hasOwnProperty('errorCode') && (0 == result.errorCode) ) {
+          app.loadSensorsData();
+        }
+        else {
+          alert('Ping request failed.');
+        }
+    },
+
+    requestError : function(xhr) {
+      alert("Error: " + xhr.status + " " + xhr.statusText);
+    },
+
+    loadSensorsData : function() {
+      $.ajax({ url: "http://iot.anavi.org/sensors/temperature",
+        success: app.loadSensorsTemperature,
+        error: app.requestError
+      });
+    },
+
+    loadSensorsTemperature : function(data) {
+        var result = JSON.parse(data);
+        var info = "";
+        for (var property in result.data) {
+          info += property + ": " + result.data[property] + "C <br />\n";
+        }
+        $("#text").html(info);
     },
 
     // Update DOM on a Received Event
